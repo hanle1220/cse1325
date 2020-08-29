f9e955c0d2bc5c1effc628e07ffed639430a95fd
Han Le <han.le2@mavs.uta.edu>
2020-08-28 23:48:31 -0500

    message

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name; 
+
+    cout << "Please enter your name: ";
+    cin >> name; 
+    cout << "Hello, " << name << "!" << endl;
+}  
